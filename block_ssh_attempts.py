bfa423ef51da94c8c008a6949cd2964d4f63c6cd
Ian Van Linden <Ian@localhost.localdomain>
2021-08-01 14:06:53 +0200

    Version 1.2 incorporates the code to look for an ipv4 or ipv6 addres in the lines that contain the words "Invalid user". If such a line is found it will save the string in front of the word port (which is the ip address) to a variable and then add  it to the ip_dict if not present with a count of 1. If present the count for this ip address will be increased by 1.

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -10,7 +10,22 @@ file_object = open("sshdlog", "r")
 #Create ip dictionary
 ip_dict = {}
 
-
+#Search for "Invalid user" in file
+for line in file_object:
+	if "Invalid user" in line:
+		#Split up the line
+		split_line = line.split()
+		#Save string in front of "port" (should be ip-address) in string_var
+		string_var = split_line[split_line("port") - 1]
+		#Check if string_var contains an IPv4 or IPv6 address
+		if "????.????.????.????" or "????:????:????:????:????:????:????:????" in String_var:
+		ip_address = string_var
+		#Add ip_address to ip_dict if not present
+		if ip_address not in ip_dict:
+			ip_dict[ip_address] = 1
+		#If present add 1 to count
+		else:
+			ip_dict[ip_address] += 1
 		
 
 		
