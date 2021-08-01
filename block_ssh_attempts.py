53187e0d1b435c7a1dd9c126e7919f2d48d1a92d
Ian Van Linden <Ian@localhost.localdomain>
2021-08-01 14:03:29 +0200

    Version 1.1 adds the fwblock module for later use, allongside the creation of the file_object variable (which contains the file that will be checked) and an ip dictionary.

--- block_ssh_attempts.py
+++ block_ssh_attempts.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 
+#Import modules
 import sys
+import fwblock #Firewall blocking module
+
+#Open file containig login attempts
+file_object = open("sshdlog", "r")
+
+#Create ip dictionary
+ip_dict = {}
+
+
+		
+
+		
+
